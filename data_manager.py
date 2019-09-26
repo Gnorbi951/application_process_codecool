4668384612f030937a200772653c7e1ea1985673
Gocze Norbi <gnorbi951@gmail.com>
2019-09-26 13:16:06 +0200

    Add all_mentor_names and miskolc_nicknames

--- data_manager.py
+++ data_manager.py
@@ -2,11 +2,21 @@ import database_common
 
 
 @database_common.connection_handler
-def get_mentor_names_by_first_name(cursor, first_name):
+def get_all_mentor_names(cursor):
     cursor.execute("""
                     SELECT first_name, last_name FROM mentors
-                    WHERE first_name = %(first_name)s ORDER BY first_name;
-                   """,
-                   {'first_name': first_name})
+                    ORDER BY first_name;
+                   """,)
     names = cursor.fetchall()
     return names
+
+
+@database_common.connection_handler
+def get_miskolc_mentor_nicknames(cursor):
+    cursor.execute("""
+                    SELECT nick_name FROM mentors
+                    WHERE city = 'Miskolc'
+                    ORDER BY nick_name;
+                    """,)
+    nicknames = cursor.fetchall()
+    return nicknames
--- main.py
+++ main.py
@@ -9,12 +9,19 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/mentors-with-best-first-name')
+@app.route('/name_of_all_mentors')
 def mentor_names():
     # We get back dictionaries here (for details check 'database_common.py')
-    mentor_names = data_manager.get_mentor_names_by_first_name('László')
+    mentor_names = data_manager.get_all_mentor_names()
 
     return render_template('mentor_names.html', mentor_names=mentor_names)
+
+
+@app.route('/miskolc_mentor_nicknames')
+def miskolc_mentor_nicknames():
+    nicknames = data_manager.get_miskolc_mentor_nicknames()
+
+    return render_template('mentor_names.html', nicknames=nicknames)
 
 
 if __name__ == '__main__':
